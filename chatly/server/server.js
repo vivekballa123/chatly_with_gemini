c8c4a0fda232895098f6d099780bef23f81734be
Vivek Balla <vivekballa123@email.com>
2026-05-04 20:43:50 +0530

    changing port condition

--- chatly/server/server.js
+++ chatly/server/server.js
@@ -58,10 +58,8 @@ app.use("/api/ai",aiRouter)
 //connect to MongoDb
 await connectDB();
 
-if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000
-    server.listen(PORT,()=> console.log("Server is running on PORT: "+ PORT))
-}
+const PORT = process.env.PORT || 5000
+server.listen(PORT,()=> console.log("Server is running on PORT: "+ PORT))
 
 
 // Export server for versel
