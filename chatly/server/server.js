3502e9da953cf168ae90b6a861ed14476fe56b3c
Vivek Balla <vivekballa123@email.com>
2026-05-19 13:26:26 +0530

    changed the path

--- chatly/server/server.js
+++ chatly/server/server.js
@@ -64,8 +64,9 @@ app.use("/api/ai",aiRouter)
 
 // Serve React frontend
 app.use(express.static(path.join(__dirname, "../../client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("/{*path}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 
