8c62fe7e3516ba5e0483cfb53f6294296e38c3dc
Vivek Balla <vivekballa123@email.com>
2026-05-19 13:16:32 +0530

    Replacing Express four syntax with express five.

--- chatly/server/server.js
+++ chatly/server/server.js
@@ -64,7 +64,7 @@ app.use("/api/ai",aiRouter)
 
 // Serve React frontend
 app.use(express.static(path.join(__dirname, "../../client/dist")));
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
     res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
 });
 
